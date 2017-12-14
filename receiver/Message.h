d2487be3bb4fdc333fb42e6a9a06f473fffddb27
MrJia1997 <jiayh_thu@outlook.com>
2017-12-14 20:00:45 +0800

    Modify message format

--- receiver/Message.h
+++ receiver/Message.h
@@ -1,10 +1,15 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
-typedef nx_struct Temperature_Msg {
+typedef nx_struct Sensor_Msg {
+    nx_uint16_t nodeid;
+    nx_uint16_t seqNumber;
+    nx_uint16_t collectTime;
     nx_uint16_t temperature;
-} Temperature_Msg;
+    nx_uint16_t humidity;
+    nx_uint16_t lightIntensity;
+} Sensor_Msg;
 
-enum {AM_TEMPERATURE_MSG = 6};
+enum {AM_SENSOR_MSG = 85};
 
 #endif
